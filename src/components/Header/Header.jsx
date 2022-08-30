8138e4757eb93925c74ea2a1f2b63fedd3e16917
thekidjnr <thejnrkid@gmail.com>
2022-08-30 09:09:23 +0000

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,22 +2,25 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <div className='bg-header bg-cover h-screen '>
-        <div className='flex pt-[20px] justify-center items-center w-11/12 m-auto max-w-[1400px]'>
-            <div className='flex justify-between w-full items-center gap-10'>
-            <h1 className='font-Rossans font-normal text-[30px] leading-[40.49px] text-[#FFCE6F] ml-[23px]'>VStores</h1>
-            <div className=' flex '>
-            <h1 className='text-white mr-[48px] font-Poppins font-normal text-[18px] leading-[27px] hover:cursor-pointer'>How it works</h1>
-            <h1 className='text-white mr-[48px] font-Poppins font-normal text-[18px] leading-[27px] hover:cursor-pointer'>Benefits</h1>
-            <h1 className='text-white mr-[48px] font-Poppins font-normal text-[18px] leading-[27px] hover:cursor-pointer'>Reviews</h1>
-            <h1 className='text-white mr-[48px] font-Poppins font-normal text-[18px] leading-[27px] hover:cursor-pointer'>Features</h1>
-            <h1 className='text-white mr-[48px] font-Poppins font-normal text-[18px] leading-[27px] hover:cursor-pointer'>FAQs</h1>
-
-            </div>
-            <div className='flex  items-center'>
-                <h1 className='hover:cursor-pointer mr-[40px] text-white font-Poppins font-normal text-[15px] leading-[22.5px]'>Log in</h1>
-                <div className='hover:cursor-pointer rounded-[7px] bg-white text-[#966507] font-Poppins font-normal text-[15px] leading-[22px] py-[12px] px-[22px]'>Start Selling</div>
-            </div>
+    <div className="bg-header bg-cover h-screen ">
+      <div className="flex pt-[20px] justify-center items-center w-11/12 m-auto max-w-[1400px]">
+        <div className="flex justify-between w-full items-center gap-10">
+          <h1 className="font-Rossans font-normal text-[30px] leading-[40.49px] text-[#FFCE6F] ml-[23px]">
+            VStores
+          </h1>
+          <div className=" flex gap-20 text-white font-Poppins font-normal text-[18px] leading-[27px] hover:cursor-pointer ">
+            <h1 className="">How it works</h1>
+            <h1 className="">Benefits</h1>
+            <h1 className="">Reviews</h1>
+            <h1 className="">Features</h1>
+            <h1 className="">FAQs</h1>
+          </div>
+          <div className="flex  items-center">
+            <h1 className="hover:cursor-pointer mr-[40px] text-white font-Poppins font-normal text-[15px] leading-[22.5px]">
+              Log in
+            </h1>
+            <div className="hover:cursor-pointer rounded-[7px] bg-white text-[#966507] font-Poppins font-normal text-[15px] leading-[22px] py-[12px] px-[22px]">
+              Start Selling
             </div>
           </div>
         </div>
